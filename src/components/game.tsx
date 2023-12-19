606a1b3e6b5896c2498370aad9d4349041b40ef9
Bryan Jennings <bryanjenningz@gmail.com>
2023-12-19 03:12:00 -0800

    Horizontally center the level

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -27,37 +27,39 @@ export function Game() {
   useKeys({ keysDown });
 
   return (
-    <div className="flex flex-col gap-2">
-      <LevelMap />
+    <div className="flex flex-col items-center gap-2">
+      <div className="relative">
+        <LevelMap />
 
-      <div
-        className="relative bg-slate-800"
-        style={{ width: screenWidth, height: screenWidth }}
-      >
         <div
-          className="absolute z-10 bg-blue-500"
-          style={{ width: blockWidth, height: blockWidth, left: x, top: y }}
-        ></div>
+          className="relative bg-slate-800"
+          style={{ width: screenWidth, height: screenWidth }}
+        >
+          <div
+            className="absolute z-10 bg-blue-500"
+            style={{ width: blockWidth, height: blockWidth, left: x, top: y }}
+          ></div>
 
-        {kanjiMonsters.map((kanjiMonster) => {
-          return (
-            <div
-              key={kanjiMonster.id}
-              className="absolute z-10 flex items-center justify-center bg-black text-white"
-              style={{
-                width: blockWidth,
-                height: blockWidth,
-                left: kanjiMonster.position.x,
-                top: kanjiMonster.position.y,
-              }}
-            >
-              {kanjiMonster.kanjiValue.kanji}
-            </div>
-          );
-        })}
+          {kanjiMonsters.map((kanjiMonster) => {
+            return (
+              <div
+                key={kanjiMonster.id}
+                className="absolute z-10 flex items-center justify-center bg-black text-white"
+                style={{
+                  width: blockWidth,
+                  height: blockWidth,
+                  left: kanjiMonster.position.x,
+                  top: kanjiMonster.position.y,
+                }}
+              >
+                {kanjiMonster.kanjiValue.kanji}
+              </div>
+            );
+          })}
+        </div>
       </div>
 
-      <div className="text-center text-xl text-slate-300">
+      <div className="w-full max-w-2xl text-center text-xl text-slate-300">
         {`Eat the kanji named "${target.meaning}"`}
       </div>
     </div>
